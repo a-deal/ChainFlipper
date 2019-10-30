a7f204ce437bf35c42dc4ff1b0bc9d9d191ad052
Andrew Deal <deal.e.andrew@gmail.com>
2019-10-30 23:00:27 +0900

    Refactor action export

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,8 +1,8 @@
 import { EventActions } from "@drizzle/store"
 
+const { EVENT_FIRED } = EventActions
 const PREDICTION_MADE = "PREDICTION_MADE"
 const PREDICTION_LOGGED = "LogPrediction"
 const RESULT_LOGGED = "LogResult"
-const EVENT_FIRED = EventActions.EVENT_FIRED
 
 export { EVENT_FIRED, PREDICTION_MADE, PREDICTION_LOGGED, RESULT_LOGGED }
